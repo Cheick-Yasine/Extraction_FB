9729f4327b6d07f2771ae42a8c7022be2042d9c4
Cheick-Yasine <kaborecheickabdineyasine@gmail.com>
2026-09-10 12:24:59 +0000

    feat(domain): package

---
+++ src/extraction_fb/domain/__init__.py
@@ -0,0 +1,19 @@
+from extraction_fb.domain.models import (
+    AccountHealth,
+    Annonce,
+    CircuitState,
+    Groupe,
+    PostBrut,
+    RunMetrics,
+    RunStatus,
+)
+
+__all__ = [
+    "AccountHealth",
+    "Annonce",
+    "CircuitState",
+    "Groupe",
+    "PostBrut",
+    "RunMetrics",
+    "RunStatus",
+]
